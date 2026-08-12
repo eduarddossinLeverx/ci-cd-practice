bfd669b1f5eb0904faec4b9a757dacdc191890c4
EduardDossin <eduard.dossin@leverx.com>
2026-08-12 23:25:59 +0300

    chore: intentional lint error

---
+++ tests/calculator.test.js
@@ -0,0 +1,16 @@
+const { add, divide } = require('../src/calculator');
+
+describe('Calculator', () => {
+  test('adds 2 + 3 to equal 5', () => {
+    expect(add(2, 3)).toBe(5);
+    expect(add(-1, 1)).toBe(0);
+  });
+
+  test('divides 10 / 2 to equal 5', () => {
+    expect(divide(10, 2)).toBe(5);
+  });
+
+  test('throws error when dividing by zero', () => {
+    expect(() => divide(5, 0)).toThrow("Cannot divide by zero!");
+  });
+});
